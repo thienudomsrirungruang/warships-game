d12230ba855699d10921212813c531be57481ea0
Thien Udomsrirungruang <thien.udomsrirungruang@gmail.com>
2020-04-24 13:52:37 +0700

    Revamped client temporary terminal

--- client/main.py
+++ client/main.py
@@ -12,6 +12,8 @@ import shutil # shutil.get_terminal_size()
 
 from settings import *
 
+move_up_next = False
+
 def main(server_ip, server_port):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,7 +30,8 @@ def send(sock):
         start_new_thread(receive, (sock,))
 
         while True:
-            message = input(">> ")
+            message = sys.stdin.readline().strip()
+            move_up_next = True
             sock.send(message.encode('utf-8'))
         sock.close()
     except ConnectionError as e:
@@ -39,8 +42,10 @@ def receive(sock):
     # print("INFO: new thread started")
     while True:
         data = sock.recv(1024)
-        sys.stdout.write("\r{}\n>> ".format(data.decode('utf-8')))
-
+        if move_up_next:
+            sys.stdout.write("\r\033[A{}\n>> ".format(data.decode('utf-8')))
+        else:
+            sys.stdout.write("\r{}\n>> ".format(data.decode('utf-8')))
 
 if __name__ == '__main__':
     main(SERVER_IP, SERVER_PORT)
