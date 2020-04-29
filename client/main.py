9d6e6ad92d53026cda5675688555924a7af3d26c
Thien Udomsrirungruang <thien.udomsrirungruang@gmail.com>
2020-04-29 09:37:10 +0700

    Added basic input parsing

--- client/main.py
+++ client/main.py
@@ -18,20 +18,22 @@ import queue
 SHIP_LENGTHS = (5, 4, 3, 3, 2)
 NUM_SHIPS = len(SHIP_LENGTHS)
 
-input_queue_lock = threading.Lock()
-input_queue = queue.Queue()
+network_queue_lock = threading.Lock()
+network_input_queue = queue.Queue()
+network_output_queue = queue.Queue()
 
-output_queue_lock = threading.Lock()
-output_queue = queue.Queue()
+chat_lock = threading.Lock()
+chat_list = []
 
 def send(sock):
     try:
         while True:
-            output_queue_lock.acquire()
-            while not output_queue.empty():
-                message = output_queue.get()
+            network_queue_lock.acquire()
+            while not network_output_queue.empty():
+                message = network_output_queue.get()
                 sock.send(message.encode('utf-8'))
-            output_queue_lock.release()
+            network_queue_lock.release()
+            time.sleep(.1)
     except ConnectionError as e:
         print("ERROR: {}".format(e))
     sock.close()
@@ -41,9 +43,35 @@ def receive(sock):
     # print("INFO: new thread started")
     while True:
         data = sock.recv(1024)
-        input_queue_lock.acquire()
-        input_queue.put(data)
-        input_queue_lock.release()
+        network_queue_lock.acquire()
+        network_input_queue.put(data.decode('utf-8'))
+        network_queue_lock.release()
+
+def start_client():
+    match = None
+    in_matchmaking = False
+    while True:
+        network_queue_lock.acquire()
+        while not network_input_queue.empty():
+            message = network_input_queue.get()
+            split_input = message.split(" ")
+            if split_input[0] == "chat":
+                chat_lock.acquire()
+                chat_list.append(" ".join(split_input[1:]))
+                chat_lock.release()
+            elif split_input[0] == "matchmake":
+                if split_input[1] == "join":
+                    in_matchmaking = True
+                elif split_input[1] == "leave":
+                    in_matchmaking = False
+                elif split_input[2] == "match":
+                    #TODO
+                    pass
+                else:
+                    print("WARN: incorrect command {}".format(message))
+            else:
+                print("WARN: incorrect command {}".format(message))
+        network_queue_lock.release()
 
 def main(server_ip, server_port):
     try:
